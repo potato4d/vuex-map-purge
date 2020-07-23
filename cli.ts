69f8fe8110266931d4e016192321729f5d739299
Takuma HANATANI <mail@potato4d.me>
2020-07-24 01:48:18 +0900

    feat: Write output

--- cli.ts
+++ cli.ts
@@ -4,40 +4,29 @@ import _glob from 'glob'
 import { promisify } from 'util'
 import { promises as fs } from 'fs'
 const glob = promisify(_glob)
-import { purgeMapGetters } from './transformers/purgeMapGetters'
-import { purgeMapMutations } from './transformers/purgeMapMutations'
-import { purgeMapActions } from './transformers/purgeMapActions'
+import * as Core from './core'
 
 async function run() {
   const files = await glob(`${process.argv[2]}`)
   await Promise.all([
     files.map(async (path) => {
+      console.log(path)
       const sfc = await fs.readFile(path, { encoding: 'utf8' })
       const $ = cheerio.load(sfc)
       const src = $('script')!.html()
       if (!src) {
         return
       }
-      const sourceFile = ts.createSourceFile(
-        'index.ts',
-        src,
-        ts.ScriptTarget.ESNext
+      const output = Core.purge(src)
+      const regexp = new RegExp('<script( lang="ts")?>((.*)\\n)*</script>')
+      // TODO: ソース内に `<script></script>` の文字があると破滅するので対処する
+      await fs.writeFile(
+        path,
+        sfc.replace(regexp, `<script$1>\n${output}</script>`),
+        {
+          encoding: 'utf8',
+        }
       )
-      const result = ts.transform(sourceFile, [
-        purgeMapGetters,
-        purgeMapMutations,
-        purgeMapActions,
-      ])
-      result.dispose()
-
-      const printer = ts.createPrinter()
-      const scriptBody = printer.printFile(
-        result.transformed[0] as ts.SourceFile
-      )
-      console.log(scriptBody)
-      // await fs.writeFile(path.replace(".vue", ".ts"), scriptBody, {
-      //   encoding: "utf8",
-      // });
     }),
   ])
 }
